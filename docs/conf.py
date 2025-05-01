01e74e1adc85425ec0242ead40dc3457004304d9
Jonathan Parkinson <jlparkinson1@gmail.com>
2025-05-01 13:58:42 -0700

    Set up docs to mock extensions.

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,8 @@ napoleon_custom_sections = [('Returns', 'params_style')]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+# These are C++ extensions that may not be installed when building
+# the docs and should therefore be "mocked".
+autodoc_mock_imports = ["resp_protein_toolkit.resp_protein_toolkit_ext",
+        "resp_protein_toolkit_ext"]
